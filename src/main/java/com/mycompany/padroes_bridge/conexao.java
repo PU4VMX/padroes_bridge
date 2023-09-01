b93a3a3a162ddcd80248335ee2686fa8d7c2b42a
server_user <server_user@vm-athon.q3tbmmg0gm1upp0bwxbf0wsgue.cx.internal.cloudapp.net>
2023-09-01 23:17:44 +0000

    Implementação inicial

---
+++ src/main/java/com/mycompany/padroes_bridge/conexao.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.padroes_bridge;
+
+/**
+ *
+ * @author server_user
+ */
+public class conexao {
+    System.out.println("olá Mundo");
+}

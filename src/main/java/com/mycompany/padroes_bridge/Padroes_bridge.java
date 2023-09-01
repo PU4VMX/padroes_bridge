672ed88401c09cee56f9ee66400f281feffe2851
server_user <server_user@vm-athon.q3tbmmg0gm1upp0bwxbf0wsgue.cx.internal.cloudapp.net>
2023-09-01 23:11:56 +0000

    Implementação inicial

---
+++ src/main/java/com/mycompany/padroes_bridge/Padroes_bridge.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.padroes_bridge;
+
+/**
+ *
+ * @author server_user
+ */
+public class Padroes_bridge {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
